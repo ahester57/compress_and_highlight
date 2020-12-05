4afeafd53e9ed2a0fda11216d49b6d40ed80b26e
Austin Hester <ahester@incomm.com>
2020-12-04 19:20:49 -0600

    draw rectangle
    Signed-off-by: Austin Hester <ahester@incomm.com>

--- highlight.cpp
+++ highlight.cpp
@@ -57,6 +57,16 @@ wait_key()
 }
 
 
+void
+do_the_thing()
+{
+    cv::Rect rect = cv::Rect(200, 200, 200, 200);
+    cv::rectangle(displayed_image, rect, cv::Scalar(5));
+    // display the new image
+    cv::imshow(WINDOW_NAME, displayed_image);
+}
+
+
 int
 main(int argc, const char** argv)
 {
@@ -78,12 +88,13 @@ main(int argc, const char** argv)
     // deep keep to displayed_image
     og_image->image.copyTo(displayed_image);
 
-    // define center
-    center = cv::Point(displayed_image.cols / 2, displayed_image.rows / 2);
-
     // display the original image
     cv::imshow(WINDOW_NAME, displayed_image);
 
+    cv::waitKey(0);
+
+    // do the thing
+    do_the_thing();
 
     // 'event loop' for keypresses
     while (wait_key());
